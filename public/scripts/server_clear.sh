6efcbfa0d5325679ac8c87818529d85dc12dba21
ippo579 <lengareshreyash@gmail.com>
2023-11-07 15:59:53 +0530

    new change

--- public/scripts/server_clear.sh
+++ public/scripts/server_clear.sh
@@ -1,15 +1,3 @@
 ##!/usr/bin/env bash
-#rm -rf /home/ubuntu
-#!/usr/bin/env bash
+rm -rf /home/ubuntu
 
-# Define the directory to clean up or prepare (replace with the appropriate path)
-app_directory="/var/www/html/"
-
-# Ensure the directory exists before attempting to clean it up or prepare it
-if [ -d "$app_directory" ]; then
-    # Clean up or prepare the directory here
-    # For example, remove old files, clear caches, etc.
-    # You can add your specific actions here.
-else
-    echo "Directory not found: $app_directory"
-fi
